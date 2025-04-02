5fcdcea52445c47b68d71e9ebe83db150662d4d6
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2025-04-02 13:23:50 +0200

    Home screen buttons

--- employee_frontend/lib/ui/home/widgets/home_screen.dart
+++ employee_frontend/lib/ui/home/widgets/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:employee_frontend/ui/core/ui/svg_circle_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -16,7 +18,7 @@ class HomeScreen extends StatelessWidget {
                   end: Alignment.bottomCenter,
                   colors: [Colors.black, Colors.transparent],
                 ).createShader(
-                  Rect.fromLTRB(0, 250, rect.width,rect.height),
+                  Rect.fromLTRB(0, 250, rect.width, rect.height),
                 );
               },
               blendMode: BlendMode.dstIn,
@@ -31,14 +33,55 @@ class HomeScreen extends StatelessWidget {
               child: Container(
                 width: double.infinity,
                 margin: EdgeInsets.only(top: 50),
-                decoration: BoxDecoration(boxShadow: [
-                  BoxShadow(
-                      blurStyle: BlurStyle.outer,
-                      blurRadius: 4,
-                      color: Colors.grey,
-                      offset: Offset(0, 1)),
-                ], borderRadius: BorderRadius.circular(10)),
-                child: Text('data'),
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        blurStyle: BlurStyle.outer,
+                        blurRadius: 4,
+                        color: Colors.grey,
+                        offset: Offset(0, 1)),
+                  ],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SvgCircleButton(
+                            labelText: 'Assigned Trains',
+                            svgAsset: 'assets/svg/map_train_station.svg',
+                          ),
+                          SvgCircleButton(
+                            labelText: 'Profile',
+                            svgAsset: 'assets/svg/profile.svg',
+                          ),
+                          SvgCircleButton(
+                            labelText: 'Tasks',
+                            svgAsset: 'assets/svg/tasks.svg',
+                          ),
+                          SvgCircleButton(
+                            labelText: 'Appraisals',
+                            svgAsset: 'assets/svg/star.svg',
+                          ),
+                        ],
+                      ),
+                       Row(
+                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SvgCircleButton(
+                            labelText: 'Citations',
+                            svgAsset: 'assets/svg/warning.svg',
+                          ),
+                        
+                        ],
+                      )
+                    ],
+                  ),
+                ),
               ),
             )
           ],
