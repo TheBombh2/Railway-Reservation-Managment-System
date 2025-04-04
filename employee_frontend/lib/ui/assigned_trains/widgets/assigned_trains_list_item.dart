7079dfcfcbda22e943b3e8122970384cf38bb77a
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2025-04-04 02:11:57 +0200

    Finished Assigned trains screen

--- employee_frontend/lib/ui/assigned_trains/widgets/assigned_trains_list_item.dart
+++ employee_frontend/lib/ui/assigned_trains/widgets/assigned_trains_list_item.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class AssignedTrainsListItem extends StatelessWidget {
   const AssignedTrainsListItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListTile(
+        leading: SvgPicture.asset('assets/svg/train.svg'),
+        contentPadding: EdgeInsets.all(10),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              'KTS/MDA-1122',
+            ),
+            Text(
+              style:
+                  TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),
+              'Aluthgama',
+            ),
+            Text(
+              style:
+                  TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),
+              '8:00 PM',
+            ),
+          ],
+        ),
+        trailing: FloatingActionButton.small(
+          onPressed: () {},
+          backgroundColor: Colors.white,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_forward_ios_rounded,
+              color: Color(
+                0xff0076CB,
+              ),
+            ),
+          ),
+        ));
   }
 }
