b47c9dc7a2b39eee59d55104dc4afd81a3a9a723
BelalSalem <62998842+TheBombh2@users.noreply.github.com>
2025-04-14 11:10:04 +0200

    Moved routing into router.dart

---
+++ employee_frontend/lib/routing/router.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:employee_frontend/ui/appraisals/widgets/appraisals_screen.dart';
+import 'package:employee_frontend/ui/assigned_trains/widgets/assigned_trains_screen.dart';
+import 'package:employee_frontend/ui/auth/widgets/login_screen.dart';
+import 'package:employee_frontend/ui/citations/widgets/citations_screen.dart';
+import 'package:employee_frontend/ui/home/widgets/home_screen.dart';
+import 'package:employee_frontend/ui/profile/widgets/profile_screen.dart';
+import 'package:employee_frontend/ui/tasks/widgets/tasks_screen.dart';
+import 'package:employee_frontend/ui/train_tracking/widgets/train_tracking_screen.dart';
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(path: '/', builder: (ctx, state) => LoginScreen()),
+    GoRoute(
+        path: '/home',
+        pageBuilder: (ctx, state) {
+          return CustomTransitionPage(
+            key: state.pageKey,
+            child: const HomeScreen(),
+            transitionsBuilder: (ctx, animation, secondartAnimation, child) {
+              const begin = Offset(1.0, 0.0);
+              const end = Offset.zero;
+              const curve = Curves.easeInOut;
+              var tween = Tween(begin: begin, end: end).chain(
+                CurveTween(
+                  curve: curve,
+                ),
+              );
+              return SlideTransition(
+                position: animation.drive(tween),
+                child: child,
+              );
+            },
+          );
+        },
+        routes: [
+          GoRoute(path: 'profile', builder: (ctx, state) => const ProfileScreen()),
+          GoRoute(path: 'tasks', builder: (ctx, state) => const TasksScreen()),
+          GoRoute(path: 'appraisals', builder: (ctx, state) => const AppraisalsScreen()),
+          GoRoute(path: 'citations', builder: (ctx, state) => const CitationsScreen()),
+          GoRoute(path: 'assigned_trains', builder: (ctx, state) => const AssignedTrainsScreen()),
+          GoRoute(path: 'train_tracking', builder: (ctx, state) => const TrainTrackingScreen()),
+        ]),
+  ],
+);
