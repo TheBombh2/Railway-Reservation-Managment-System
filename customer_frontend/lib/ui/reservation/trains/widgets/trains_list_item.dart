a4f2ee1acd61d3b926b558a78e7af1552e5888f4
Belal Salem <belalsalem500@gmail.com>
2025-04-19 17:17:41 +0200

    Modifed Trains list

--- customer_frontend/lib/ui/reservation/trains/widgets/trains_list_item.dart
+++ customer_frontend/lib/ui/reservation/trains/widgets/trains_list_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:go_router/go_router.dart';
 
 class TrainsListItem extends StatelessWidget {
   const TrainsListItem({super.key});
@@ -8,41 +7,55 @@ class TrainsListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        leading: SvgPicture.asset('assets/svg/train.svg'),
-        contentPadding: EdgeInsets.all(10),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              'KTS/MDA-1122',
-            ),
-            Text(
-              style:
-                  TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),
-              'Aluthgama',
-            ),
-            Text(
-              style:
-                  TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),
-              '8:00 PM',
-            ),
-          ],
-        ),
-        trailing: FloatingActionButton.small(
-          onPressed: () {
-            context.push('/home/train_tracking');
-            debugPrint("new screen?");
-          },
-          backgroundColor: Colors.white,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          child: Icon(
-            Icons.arrow_forward_ios_rounded,
-            color: Color(
-              0xff0076CB,
-            ),
+      leading: SvgPicture.asset('assets/svg/train.svg'),
+      contentPadding: EdgeInsets.all(10),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'KTS/MDA-1122',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Aluthgama',
+                style: TextStyle(
+                  fontSize: 13,
+                  color: Color.fromRGBO(0, 0, 0, 0.6),
+                ),
+              ),
+            ],
           ),
-        ));
+          Row(
+            children: [
+              Icon(Icons.people,color: Colors.green,),
+              SizedBox(width: 10,),
+              Text(
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Color.fromRGBO(0, 0, 0, 0.6),
+                ),
+                'Arrival Now',
+              ),
+            ],
+          ),
+        ],
+      ),
+      trailing: FloatingActionButton.small(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (ctx) {
+              return Placeholder();
+            },
+          );
+        },
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        child: Icon(Icons.arrow_forward_ios_rounded, color: Color(0xff0076CB)),
+      ),
+    );
   }
 }
